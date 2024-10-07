4f5a6345a2f8eca2f77425e63bb39838eadf3601
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-10-07 14:58:09 +0200

    fix: cookie not stored on http

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -9,7 +9,7 @@ export const lucia = new Lucia(adapter, {
   sessionCookie: {
     expires: false,
     attributes: {
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
     },
   },
   getUserAttributes: (attributes) => {
